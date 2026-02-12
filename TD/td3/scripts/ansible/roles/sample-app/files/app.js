aeac73e0cc471d5afb380026381deae2cd352c6a
Meliana Zerroug <meliana.zerroug@edu.esiee.fr>
2026-02-12 16:48:08 +0000

    modif ansible td3

--- TD/td3/scripts/ansible/roles/sample-app/files/app.js
+++ TD/td3/scripts/ansible/roles/sample-app/files/app.js
@@ -1,25 +1,7 @@
-const http = require('http');
-const os = require('os');
-
-const getPrivateIP = () => {
-  const interfaces = os.networkInterfaces();
-  for (const name of Object.keys(interfaces)) {
-    for (const iface of interfaces[name]) {
-      if (iface.family === 'IPv4' && !iface.internal) {
-        return iface.address;
-      }
-    }
-  }
-  return 'unknown';
-};
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end(`DevOps Base Server: ${getPrivateIP()}\n`);
+const http = require('http'); 
+const server = http.createServer((req, res) => { 
+  res.end('DevOps Base!\n'); 
+}); 
+server.listen(8080, () => { 
+  console.log('Listening on port 8080'); 
 });
-
-const port = process.env.PORT || 8080;
-server.listen(port,() => {
-  console.log(`Listening on port ${port}`);
-});
-
