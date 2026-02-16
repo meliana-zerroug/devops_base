0bfc4906c19fb7978faac10d4bd387361b7afea3
meliana-zerroug <meliana.zerroug@edu.esiee.fr>
2026-02-16 16:41:13 +0100

    Add infrastructure tests workflow

--- TD/td5/scripts/tofu/live/ci-cd-permissions/main.tf
+++ TD/td5/scripts/tofu/live/ci-cd-permissions/main.tf
@@ -17,14 +17,13 @@ module "iam_roles" {
 
   enable_iam_role_for_testing = true                            
 
-  # TODO: fill in your own repo name here!
-  github_repo      = "brikis98/fundamentals-of-devops-examples" 
+  github_repo      = "meliana-zerroug/devops_base" 
   lambda_base_name = "lambda-sample"                            
 
   enable_iam_role_for_plan  = true                                
   enable_iam_role_for_apply = true                                
 
-  # TODO: fill in your own bucket and table name here!
-  tofu_state_bucket         = "fundamentals-of-devops-tofu-state" 
-  tofu_state_dynamodb_table = "fundamentals-of-devops-tofu-state" 
+
+  tofu_state_bucket         = "meliana-zerroug-devops-tofu-state" 
+  tofu_state_dynamodb_table = "meliana-zerroug-devops-tofu-state" 
 }
