63f747200804fa56133c55d82e9180a6f991ed45
meliana-zerroug <meliana.zerroug@edu.esiee.fr>
2026-02-17 11:48:46 +0100

    Add IAM PutRolePolicy and DeleteRolePolicy permissions

--- TD/td5/modules/gh-actions-iam-roles/main.tf
+++ TD/td5/modules/gh-actions-iam-roles/main.tf
@@ -49,7 +49,9 @@ data "aws_iam_policy_document" "test_serverless_app" {
       "iam:UpdateRole",
       "iam:PassRole",
       "iam:List*Role*",
-      "iam:Get*Role*"
+      "iam:Get*Role*",
+      "iam:PutRolePolicy",
+      "iam:DeleteRolePolicy"
     ]
     resources = ["arn:aws:iam::*:role/${var.lambda_base_name}*"]
   }
@@ -190,7 +192,9 @@ data "aws_iam_policy_document" "apply_serverless_app" {
       "iam:UpdateRole",
       "iam:PassRole",
       "iam:List*Role*",
-      "iam:Get*Role*"
+      "iam:Get*Role*",
+      "iam:PutRolePolicy",
+      "iam:DeleteRolePolicy"
     ]
     resources = ["arn:aws:iam::*:role/${var.lambda_base_name}*"]
   }
